ab7a3913dbe05437e76eef006b87180875eb881c
howdyAnkit <palankit35@gmail.com>
2020-11-21 11:07:24 +0530

    Adding Database Mongo

---
+++ src/server.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/api/articles/:name', async (req,res) => {
+    try{
+        const articleName = req.params.name;
+
+        const client = await (MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }))
+        const db = client.db('my-blog');
+
+        const articlesInfo = await db.collection('articles').findOne({ name: articleName });
+        res.status(200).json(articlesInfo);
+
+        client.close();
+    }catch(error){
+        res.status(500).json({message: 'Error Connecting to db', error});
+    }
+});
+
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const articleName = req.params.name;
+    articlesInfo[articleName].upvotes += 1;
+    
+    res.status(200).send(`${articlesInfo[articleName].upvotes}`);
+});
+
+app.post('/api/article/:name/add-comment', (req,res) => {
+    const { username, text } = req.body;
+    const articleName = req.params.name;
+    articlesInfo[articleName].comments.push({username, text});
+    res.status(200).send(articlesInfo[articleName]);
+});
+
+// app.get('/hello', (req,res) =>{
+//     res.send('Hello');
+// });
+
+// app.get('/hello/:name', (req,res) => { res.send(`Hello ${req.params.name}`)});
+
+// app.post('/hello', (req,res) => {
+//     res.send(`Hello ${req.body.name}`);
+// });
+
+app.listen(8000, () => {
+    console.log("Listening On 8000");
+})
